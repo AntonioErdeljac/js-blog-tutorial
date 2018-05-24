fa4bbac94f9a180a549e1dd547d07a19fd735f4c
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2018-05-25 01:26:05 +0200

    Added server folder & app.js

---
+++ server/app.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose.promise = global.Promise;
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'LightBlog', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+mongoose.connect('mongodb://localhost/lightblog');
+mongoose.set('debug', true);
+
+// Add models
+// Add routes
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
